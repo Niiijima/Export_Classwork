c1ac09355f212d44f7f55fe418022d983ffa0213
Niiijima <qagwu11@gmail.com>
2026-04-29 19:13:39 +0100

    export class

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { multiply, divide } = require("./calculator");
 const name = require("./message");
 
-console.log("Name:", name);
+console.log("Name:");
 
 console.log("Multiplication:", multiply(5, 3));
 console.log("Division:", divide(10, 2));
